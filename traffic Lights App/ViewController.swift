ff9adadffbe7dbedddafe49a245ee90f79abbae7
Sergei Rud <sergejrud@gmail.com>
2022-03-02 15:33:56 +0300

    Initial Commit

---
+++ traffic Lights App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  traffic Lights App
+//
+//  Created by Сергей Рудь on 2.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
